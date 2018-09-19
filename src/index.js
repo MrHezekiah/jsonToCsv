1c29c7e724a07e8b6a001445730d31daa50cc98f
Mr. Hezekiah <giwaisaac@hotmail.com>
2018-09-19 17:42:50 +0200

    Read the file

--- src/index.js
+++ src/index.js
@@ -4,5 +4,17 @@ const pathToFile = 'static/famous.json';
 
 fs.readFile(pathToFile, (err, data) => {
     if (err) throw err;
-    console.log(data);
+    const famous = JSON.parse(data);
+
+    const head = Object.keys(famous[0]).join(',');
+    const bottom = famous.map(head => {
+        return Object.values(head).join();
+    })
+
+    fs.writeFile('static/output.csv', `${head}\n ${bottom}.join('\n')`, err => {
+        if (err) throw err;
+
+        console.log(head);
+    });
+
 });
