4ac8ed20f45e48f06ec4e6365456e8ca4b54d7e6
Mr. Hezekiah <giwaisaac@hotmail.com>
2018-09-20 16:43:46 +0200

    Made creating the csv file dynamic- executing any JSON file from the terminal

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import fs from 'fs';
 
-const pathToFile = 'static/famous.json';
+const args = process.argv.slice(2);
+console.log(args);
+// const pathToFile = 'static/famous.json';
+const pathToFile = args.toString();
 
 fs.readFile(pathToFile, (err, data) => {
     if (err) throw err;
     const famous = JSON.parse(data);
+
 
     const head = Object.keys(famous[0]).join(',');
     const bottom = famous.map(head => {
